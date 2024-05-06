4caf23af876acd44973320e198279c861038e81f
newstatic <126313818+newstatic@users.noreply.github.com>
2024-05-06 17:39:33 +0700

    Update device_info.go

--- device_info.go
+++ device_info.go
@@ -74,8 +74,10 @@ func parseDeviceLong(line string) (*DeviceInfo, error) {
 func parseDeviceAttributes(fields []string) map[string]string {
 	attrs := map[string]string{}
 	for _, field := range fields {
-		key, val := parseKeyVal(field)
-		attrs[key] = val
+		if strings.ContainsAny(field, ":") {
+			key, val := parseKeyVal(field)
+			attrs[key] = val
+		} 
 	}
 	return attrs
 }
